230d68c8b486e2aa13760b4f6a4ba2882c094c36
taha uygun <tahauygun@gmail.com>
2020-12-15 22:55:50 -0800

    validation and trim middleware

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import { User } from "./entities/User";
 import morgan from "morgan";
 import authRoutes from "./routes/auth";
 
+import trim from "./middleware/trim";
+
 import express from "express";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(trim);
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/auth", authRoutes);
