41e4f364e50deec1d1be30cadb24a66acc64fe42
taha uygun <tahauygun@gmail.com>
2020-12-15 22:58:57 -0800

    validation continues

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,9 +6,20 @@ const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
   try {
+    let errors: any = {};
+    const emailUser = await User.findOne({ email });
+    const usernameUser = await User.findOne({ username });
+
+    if (emailUser) errors.email = "Email is already taken";
+    if (usernameUser) errors.username = "Username is already taken";
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json({ errors });
+    }
+
     const user = new User({ email, username, password });
     await user.save();
-    const errors = await validate(user);
+    errors = await validate(user);
 
     if (errors.length > 0) {
       return res.status(400).json({ errors });
