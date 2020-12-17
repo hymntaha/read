33268cf02b3dcd15aa6381165a833cbb3b5c5357
taha uygun <tahauygun@gmail.com>
2020-12-16 23:36:37 -0800

    adding cookie and jwt flow

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,11 @@
 import { Request, Response, Router } from "express";
-import { validate } from "class-validator";
-import { User } from "../entities/User";
+import { isEmpty, validate } from "class-validator";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+
+import { User } from "../entities/User";
+
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
@@ -35,6 +39,15 @@ const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
   try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
     const user = await User.findOne({ username });
 
     if (!user) return res.status(404).json({ error: "User not found" });
@@ -44,11 +57,25 @@ const login = async (req: Request, res: Response) => {
       return res.status(401).json({ password: "Password is incorrect" });
     }
 
+    const token = jwt.sign({ username }, "34rwdfsd24dswfdsfs324dsfsd3sdfsdf");
+
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "strict",
+        maxAge: 3600,
+        path: "/",
+      })
+    );
+
     return res.json(user);
   } catch (err) {}
 };
 
 const router = Router();
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
