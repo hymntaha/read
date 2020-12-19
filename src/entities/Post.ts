ed99ab0115b35ddc3e39636209e9bb33345531a0
taha uygun <tahauygun@gmail.com>
2020-12-19 14:08:14 -0800

    adding create post route

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -4,6 +4,7 @@ import {
   Index,
   BeforeInsert,
   ManyToOne,
+  JoinColumn,
 } from "typeorm";
 
 import { IsEmail, Length } from "class-validator";
@@ -37,5 +38,6 @@ export default class Post extends Entity {
   subName: string;
 
   @ManyToOne(() => User, (user) => user.posts)
+  @JoinColumn({ name: "username", referencedColumnName: "username" })
   user: User;
 }
