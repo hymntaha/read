7361b705d512a644e409ea4413071c1baf60200f
taha uygun <tahauygun@gmail.com>
2020-12-22 00:09:56 -0800

    get post and get posts routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
-import morgan from "morgan";
+// import morgan from "morgan";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 import authRoutes from "./routes/auth";
@@ -16,17 +17,18 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
+
 app.use(trim);
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (_, res) => res.send("Hello World"));
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/subs", subRoutes);
 
 app.listen(PORT, async () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 
   try {
     await createConnection();
