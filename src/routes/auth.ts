4629f845911e9f94ffe9867073ab219a6dafb01d
taha uygun <tahauygun@gmail.com>
2020-12-21 23:55:26 -0800

    some cleaning up

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -58,7 +58,7 @@ const login = async (req: Request, res: Response) => {
       return res.status(401).json({ password: "Password is incorrect" });
     }
 
-    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+    const token = jwt.sign({ username }, process.env.JWT_SECRET!);
 
     res.set(
       "Set-Cookie",
