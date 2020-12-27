4239d8d2643ae066110b52615406121a6d5d33a6
taha uygun <tahauygun@gmail.com>
2020-12-26 16:28:44 -0800

    adding client and ts config

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Register() {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +12,17 @@ export default function Register() {
       <main className={styles.main}>
         <h1 className={styles.title}>Register Page</h1>
       </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
     </div>
   );
 }
