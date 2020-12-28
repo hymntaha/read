57124518872d049962ec5679965f445a433cacd9
taha uygun <tahauygun@gmail.com>
2020-12-27 23:35:09 -0800

    setup for spacing

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -13,6 +13,12 @@ export default function Home() {
         className="w-40 h-screen bg-center bg-cover"
         style={{ backgroundImage: "url('/images/bricks.jpg)" }}
       ></div>
+      <div className="flex flex-col justify-center pl-6">
+        <h1 className="mb-2 text-lg">Sign Up</h1>
+        <p className="mb-10 text-sm">
+          By continiuing you agree to our User Agreement and Privacy Policy
+        </p>
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./src/**/*.tsx", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        70: "17.5",
+      },
+    },
   },
   variants: {
     extend: {},
