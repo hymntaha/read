043db5bbee14347decb7702c59dd5765fa92081a
taha uygun <tahauygun@gmail.com>
2020-12-27 23:29:57 -0800

    work on register page

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -3,26 +3,16 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex">
       <Head>
-        <title>Create Next App</title>
+        <title>Register</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Register Page</h1>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div
+        className="w-40 h-screen bg-center bg-cover"
+        style={{ backgroundImage: "url('/images/bricks.jpg)" }}
+      ></div>
     </div>
   );
 }
