a1a9ea3eef358ba17584d4a2b5d554a690e35014
taha uygun <tahauygun@gmail.com>
2020-12-28 20:20:28 -0800

    some changes on register page

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -35,21 +35,21 @@ export default function Home() {
             <div className="mb-2">
               <input
                 type="email"
-                className="w-full p-3 py-2 border border-gray-400 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white"
+                className="w-full p-3 py-2 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white"
                 placeholder="EMAIL"
               />
             </div>
             <div className="mb-2">
               <input
                 type="text"
-                className="w-full p-3 py-2 border border-gray-400 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white"
+                className="w-full p-3 py-2 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white"
                 placeholder="USERNAME"
               />
             </div>
             <div className="mb-2">
               <input
                 type="password"
-                className="w-full p-3 py-2 border border-gray-400 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white"
+                className="w-full p-3 py-2 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white"
                 placeholder="PASSWORD"
               />
             </div>
