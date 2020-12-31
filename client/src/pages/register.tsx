313f33e711201d2f506666b73a903e4aff8e0970
taha uygun <tahauygun@gmail.com>
2020-12-30 20:35:55 -0800

    react validation changes

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
+
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="flex">
