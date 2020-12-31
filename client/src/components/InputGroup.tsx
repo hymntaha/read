4e5cfe1fc52066f77d78f81a4494926fa62c6195
taha uygun <tahauygun@gmail.com>
2020-12-31 02:00:57 -0800

    refactoring input groups

--- client/src/components/InputGroup.tsx
+++ client/src/components/InputGroup.tsx
@@ -1,19 +1,35 @@
 import classNames from "classnames";
 
-const InputGroup: React.FC = () => {
+interface InputGroupProps {
+  className?: string;
+  type: string;
+  placeholder: string;
+  value: string;
+  error: string | undefined;
+  setValue: (str: string) => void;
+}
+
+const InputGroup: React.FC<InputGroupProps> = ({
+  className,
+  type,
+  placeholder,
+  value,
+  error,
+  setValue,
+}) => {
   return (
-    <div className="mb-2">
+    <div className={classNames}>
       <input
-        type="email"
+        type={type}
         className={classNames(
           "w-full p-3 py-2 duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white",
-          { "border-red-500": errors.email }
+          { "border-red-500": error }
         )}
-        placeholder="EMAIL"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
-      <small className="font-medium text-red-600">{errors.email}</small>
+      <small className="font-medium text-red-600">{error}</small>
     </div>
   );
 };
