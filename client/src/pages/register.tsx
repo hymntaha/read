29287ce22fb66062823c5e249242dc4e1e96f74e
taha uygun <tahauygun@gmail.com>
2020-12-31 01:49:28 -0800

    error styling

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -68,6 +68,7 @@ export default function Home() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
+              <small className="font-medium text-red-600">{errors.email}</small>
             </div>
             <div className="mb-2">
               <input
