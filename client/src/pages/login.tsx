ddabb0f7a83a898a6acdc63633da419f99c8add3
taha uygun <tahauygun@gmail.com>
2020-12-31 02:28:57 -0800

    some changes with axios credentials

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -19,14 +19,10 @@ export default function Home() {
     event.preventDefault();
 
     try {
-      await Axios.post(
-        "/auth/login",
-        {
-          username,
-          password,
-        },
-        { withCredentials: true }
-      );
+      await Axios.post("/auth/login", {
+        username,
+        password,
+      });
 
       router.push("/");
     } catch (error) {
