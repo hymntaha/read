30a67ae1fcdca1efd14bbee9c258ce1513bad7e0
taha uygun <tahauygun@gmail.com>
2021-01-01 23:55:52 -0800

    adding logo

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+import TacotacoLogo from "../images/reddit.svg";
 
 export default function Home() {
   return (
@@ -7,7 +10,15 @@ export default function Home() {
       <Head>
         <title>tacotaco: the front page of the internet</title>
       </Head>
-      div.fixed.inset-x-0.top-0.z-10
+      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12">
+        <div className="flex items-center">
+          <Link href="/">
+            <a href="">
+              <TacotacoLogo className="w-8 h-8 mr-2" />
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
