8db9d6a28314121f395fa46494cb061c74be6a5a
taha uygun <tahauygun@gmail.com>
2021-01-02 13:49:57 -0800

    navbar

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>tacotaco: the front page of the internet</title>
       </Head>
-      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white">
+      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
         <div className="flex items-center">
           <Link href="/">
             <a href="">
@@ -25,14 +25,17 @@ export default function Home() {
           <input
             type="text"
             className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+            placeholder="Search"
           />
         </div>
         <div className="flex">
           <Link href="/login">
-            <a className="holllow blue botton">log in</a>
+            <a className="w-32 py-1 mr-4 leading-5 holllow blue botton">
+              log in
+            </a>
           </Link>
           <Link href="/register">
-            <a className=" blue botton">sign up</a>
+            <a className="w-32 py-1 leading-5 blue botton">sign up</a>
           </Link>
         </div>
       </div>
