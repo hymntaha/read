0642d16c7b9eec3d82641ddc3801e2a280e36d02
taha uygun <tahauygun@gmail.com>
2021-01-02 13:38:47 -0800

    css stuff

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
         <title>tacotaco: the front page of the internet</title>
       </Head>
-      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12">
+      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white">
         <div className="flex items-center">
           <Link href="/">
             <a href="">
@@ -20,6 +20,13 @@ export default function Home() {
           <span className="text-2xl font-semibold">
             <Link href="/">tacotaco</Link>
           </span>
+        </div>
+        <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+          <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
+          <input
+            type="text"
+            className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+          />
         </div>
       </div>
     </div>
