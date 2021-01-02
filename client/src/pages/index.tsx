dff387695991485ccba7a7d0d72c391971ca92ed
taha uygun <tahauygun@gmail.com>
2021-01-02 13:53:22 -0800

    refactoring

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -9,36 +9,6 @@ export default function Home() {
       <Head>
         <title>tacotaco: the front page of the internet</title>
       </Head>
-      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
-        <div className="flex items-center">
-          <Link href="/">
-            <a href="">
-              <TacotacoLogo className="w-8 h-8 mr-2" />
-            </a>
-          </Link>
-          <span className="text-2xl font-semibold">
-            <Link href="/">tacotaco</Link>
-          </span>
-        </div>
-        <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
-          <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
-          <input
-            type="text"
-            className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
-            placeholder="Search"
-          />
-        </div>
-        <div className="flex">
-          <Link href="/login">
-            <a className="w-32 py-1 mr-4 leading-5 holllow blue botton">
-              log in
-            </a>
-          </Link>
-          <Link href="/register">
-            <a className="w-32 py-1 leading-5 blue botton">sign up</a>
-          </Link>
-        </div>
-      </div>
     </div>
   );
 }
