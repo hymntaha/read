4f85a824b64bfddc4b2e1845dfe382d065c5c6d9
taha uygun <tahauygun@gmail.com>
2021-01-03 21:58:05 -0800

    fetching posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head";
 
+import { useEffect } from "react";
+import Axios from "axios";
+
 export default function Home() {
+  useEffect(() => {
+    effect;
+    return () => {
+      cleanup;
+    };
+  }, [input]);
   return (
     <div className="pt-12">
       <Head>
         <title>tacotaco: the front page of the internet</title>
       </Head>
-      <div className="container">
-        <h1>Recent Posts</h1>
+      <div className="container flex pt-4">
+        <div className="w-160"></div>
       </div>
     </div>
   );
