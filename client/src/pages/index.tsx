a5b6db2343ef24dfa2aa69a2bc01083fc2d342d2
taha uygun <tahauygun@gmail.com>
2021-01-03 21:59:32 -0800

    fetching posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,22 +1,27 @@
 import Head from "next/head";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
-    effect;
-    return () => {
-      cleanup;
-    };
-  }, [input]);
+    Axios.get("/posts")
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="pt-12">
       <Head>
         <title>tacotaco: the front page of the internet</title>
       </Head>
       <div className="container flex pt-4">
-        <div className="w-160"></div>
+        <div className="w-160">
+          {posts.map((post) => (
+            <div key={post.identifier}></div>
+          ))}
+        </div>
       </div>
     </div>
   );
