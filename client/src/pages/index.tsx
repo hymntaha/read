7ddb54c99c3ace0bab9d93e957351853f30b4ac0
taha uygun <tahauygun@gmail.com>
2021-01-03 21:54:40 -0800

    tailwind container config

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,10 +2,13 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="pt-12">
       <Head>
         <title>tacotaco: the front page of the internet</title>
       </Head>
+      <div className="container">
+        <h1>Recent Posts</h1>
+      </div>
     </div>
   );
 }
