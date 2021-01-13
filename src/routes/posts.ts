1701be2d75f3838da007330f440f90c8a20f4359
taha uygun <tahauygun@gmail.com>
2021-01-12 22:14:28 -0800

    some refactoring on middlewares

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -4,6 +4,7 @@ import Post from "../entities/Post";
 import Sub from "../entities/Sub";
 
 import auth from "../middleware/auth";
+import user from "../middleware/user";
 
 const createPost = async (req: Request, res: Response) => {
   const { title, body, sub } = req.body;
@@ -82,9 +83,9 @@ const commentOnPost = async (req: Request, res: Response) => {
 
 const router = Router();
 
-router.post("/", auth, createPost);
-router.get("/", getPosts);
+router.post("/", user, auth, createPost);
+router.get("/", user, getPosts);
 router.get("/:identifier/:slug", getPost);
-router.post("/:identifier/:slug/comments", auth, commentOnPost);
+router.post("/:identifier/:slug/comments", user, auth, commentOnPost);
 
 export default router;
