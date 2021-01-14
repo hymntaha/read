213e92c7e3a7f00b789bcff25d9363c5051ead59
taha uygun <tahauygun@gmail.com>
2021-01-13 22:33:32 -0800

    navbar changes based on auth context

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,38 +1,52 @@
+import { Fragment } from "react";
 import Link from "next/link";
-
 import { useAuthState, useAuthDispatch } from "../context/auth";
 
 import TacotacoLogo from "../images/reddit.svg";
 
-const Navbar: React.FC = () => (
-  <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
-    <div className="flex items-center">
-      <Link href="/">
-        <a href="">
-          <TacotacoLogo className="w-8 h-8 mr-2" />
-        </a>
-      </Link>
-      <span className="text-2xl font-semibold">
-        <Link href="/">tacotaco</Link>
-      </span>
+const Navbar: React.FC = () => {
+  const { authenticated } = useAuthState();
+
+  return (
+    <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
+      <div className="flex items-center">
+        <Link href="/">
+          <a href="">
+            <TacotacoLogo className="w-8 h-8 mr-2" />
+          </a>
+        </Link>
+        <span className="text-2xl font-semibold">
+          <Link href="/">tacotaco</Link>
+        </span>
+      </div>
+      <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+        <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
+        <input
+          type="text"
+          className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+          placeholder="Search"
+        />
+      </div>
+      <div className="flex">
+        {authenticated ? (
+          <button className="w-32 py-1 mr-4 leading-5 holllow blue botton">
+            logout
+          </button>
+        ) : (
+          <Fragment>
+            <Link href="/login">
+              <a className="w-32 py-1 mr-4 leading-5 holllow blue botton">
+                log in
+              </a>
+            </Link>
+            <Link href="/register">
+              <a className="w-32 py-1 leading-5 blue botton">sign up</a>
+            </Link>
+          </Fragment>
+        )}
+      </div>
     </div>
-    <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
-      <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
-      <input
-        type="text"
-        className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
-        placeholder="Search"
-      />
-    </div>
-    <div className="flex">
-      <Link href="/login">
-        <a className="w-32 py-1 mr-4 leading-5 holllow blue botton">log in</a>
-      </Link>
-      <Link href="/register">
-        <a className="w-32 py-1 leading-5 blue botton">sign up</a>
-      </Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
