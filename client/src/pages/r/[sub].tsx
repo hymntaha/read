08b96b39d8cb991ad19ed718189ad84e54c6c486
taha uygun <tahauygun@gmail.com>
2021-01-17 21:35:45 -0800

    taking care some edge cases

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -9,15 +9,22 @@ export default function Sub() {
 
   const { data: sub } = useSWR(subName ? `/subs/${subName}` : null);
 
+  let postsMarkup;
+
+  if (!sub) {
+    postsMarkup = <p className="text-lg text-center">Loading...</p>;
+  } else if (sub.posts.length === 0) {
+    postsMarkup = (
+      <p className="text-lg text-center">No posts submitted yet...</p>
+    );
+  } else {
+    postsMarkup = sub.posts.map((post) => (
+      <PostCard key={post.identifier} post={post} />
+    ));
+  }
   return (
     <div className="container flex pt-5">
-      {sub && (
-        <div className="w-160">
-          {sub.posts.map((post) => (
-            <PostCard key={post.identifier} post={post} />
-          ))}
-        </div>
-      )}
+      {sub && <div className="w-160">{postsMarkup}</div>}
     </div>
   );
 }
