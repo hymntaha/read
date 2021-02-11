6c38329d12fcefecd9d518966b5d50dfcf547bcc
Taha Uygun <tahauygun@gmail.com>
2021-02-10 20:55:17 -0800

    addin top sub

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -7,6 +7,7 @@ import Vote from "../entities/Vote";
 
 import auth from "../middleware/auth";
 import user from "../middleware/user";
+import { getConnection } from "typeorm";
 
 const vote = async (req: Request, res: Response) => {
   const { identifier, slug, commentIdentifier, value } = req.body;
@@ -64,9 +65,35 @@ const vote = async (req: Request, res: Response) => {
   }
 };
 
-const topSubs = async (req: Request, res: Response) => {
+const topSubs = async (_: Request, res: Response) => {
   try {
-  } catch (err) {}
+    /**
+     * SELECT s.title, s.name,
+     * COALESCE('http://localhost:5000/images/' || s."imageUrn" , 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y') as imageUrl,
+     * count(p.id) as "postCount"
+     * FROM subs s
+     * LEFT JOIN posts p ON s.name = p."subName"
+     * GROUP BY s.title, s.name, imageUrl
+     * ORDER BY "postCount" DESC
+     * LIMIT 5;
+     */
+    const imageUrlExp = `COALESCE('${process.env.APP_URL}/images/' || s."imageUrn" , 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y')`;
+    const subs = await getConnection()
+      .createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
+      )
+      .from(Sub, "s")
+      .leftJoin(Post, "p", `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, "DESC")
+      .limit(5)
+      .execute();
+
+    return res.json(subs);
+  } catch (err) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
 };
 
 const router = Router();
