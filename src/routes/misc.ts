90d28326fe033d438c31de0c39f0a8f9cabb4571
Taha Uygun <tahauygun@gmail.com>
2021-02-10 20:58:56 -0800

    adding the top sub route

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -89,5 +89,6 @@ const topSubs = async (_: Request, res: Response) => {
 
 const router = Router();
 router.post("/vote", user, auth, vote);
+router.get("/top-subs", topSubs);
 
 export default router;
